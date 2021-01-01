7c9591923067d26a82808774a49542230d15a970
Tanya Ivato <tetiana.ivato@gmail.com>
2021-01-01 18:05:35 +0200

    Test deploy

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -3,11 +3,11 @@ import MarketingApp from './components/MarketingApp';
 
 export default () => {
   return (
-    <>
+    <div>
       <div>Hello inside container</div>
       <hr />
       <MarketingApp />
-    </>
+    </div>
   )
 }
   
