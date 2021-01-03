d4b2d530de90ca7830357cd8a68c339a5b9ec1b2
Tanya Ivato <tetiana.ivato@gmail.com>
2021-01-03 16:02:09 +0200

    Fixed routing

--- packages/marketing/src/bootstrap.js
+++ packages/marketing/src/bootstrap.js
@@ -1,15 +1,30 @@
 import React from 'react';
 import { render } from 'react-dom';
+import { createMemoryHistory, createBrowserHistory } from 'history';
 import App from './App';
 
-const mount = (el) => {
-    render(<App />, el);
+const mount = (el, { onNavigate, defaultHistory }) => {
+  const history = defaultHistory || createMemoryHistory();
+
+  if (onNavigate) {
+    history.listen(onNavigate);
+  }
+    render(<App history={history} />, el);
+
+  return {
+    onParentNavigate({ pathname: nextPathname }) {
+      const { pathname } = history.location;
+      if (pathname !== nextPathname) {
+        history.push(nextPathname);
+      }
+    }
+  }
 }
 if (process.env.NODE_ENV === 'development') {
   const element = document.querySelector('#_marketing-dev-root');
 
   if (element) {
-    mount(element)
+    mount(element, { defaultHistory: createBrowserHistory() })
   }
 }
 
